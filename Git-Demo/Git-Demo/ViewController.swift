06602e04c1b47114bc7a33b3b987c55234203e72
Rahul Vattekadath <Rahul.Vattekadath@icloud.com>
2018-04-15 21:53:28 +0530

    Adding First Project

---
+++ Git-Demo/Git-Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Git-Demo
+//
+//  Created by Rahul Vattekadath on 15/04/18.
+//  Copyright © 2018 rahulvattekadath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
