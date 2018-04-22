12b6e45fbfeaf5f8dec8408e3153f1c9d6fc2515
Rahul Vattekadath <Rahul.Vattekadath@icloud.com>
2018-04-22 20:41:54 +0530

    Project To Learn About Table View

---
+++ Landmark Image Book/Landmark Image Book/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Landmark Image Book
+//
+//  Created by Rahul Vattekadath on 22/04/18.
+//  Copyright © 2018 rahulvattekadath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource=self
+        tableView.delegate=self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text="Working"
+        return cell
+    }
+}
+
